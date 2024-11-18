982981750936bba62b810181a6abccf76a033310
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2024-11-18 09:49:32 +0100

    implementacion supabase

--- app/src/main/java/com/example/crm_escuelabaile/MainActivity.kt
+++ app/src/main/java/com/example/crm_escuelabaile/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.crm_escuelabaile.controllers.NavigationWrapper
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
 
 class MainActivity : ComponentActivity() {
     private lateinit var navHostController: NavHostController
@@ -18,5 +20,11 @@ class MainActivity : ComponentActivity() {
             navHostController = rememberNavController()
             NavigationWrapper(navHostController)
         }
+    }
+    val supabase = createSupabaseClient(
+        supabaseUrl = "https://dpuzvhcgdzwbyboywjpr.supabase.co",
+        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwdXp2aGNnZHp3Ynlib3l3anByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MTY4MTIsImV4cCI6MjA0NzQ5MjgxMn0.C2wnB58ZUgNfx1G7Rr0Yu6PhPh_jxg80GCqCsyhOul8"
+    ) {
+        install(Postgrest)
     }
 }
